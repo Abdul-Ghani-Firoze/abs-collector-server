d1feffd9fab13fb20c238cb9982abcab16cbfa88
Abdul Ghani Firoze <25895690+The-Elev8r@users.noreply.github.com>
2019-03-20 18:47:21 +0530

    implemented storing product_visits and fixed connection opening and closing issue

--- service.js
+++ service.js
@@ -6,8 +6,9 @@
 
 
 var mysql = require('mysql');
+var dateFormat = require('dateformat');
 
-var con = mysql.createConnection({
+var con = mysql.createPool({
     host: "localhost",
     user: "abs",
     password: "absadmin",
@@ -26,7 +27,7 @@ function log_event(event, eventData) {
     switch (event) {
         case 'Entered product page':
             console.log("entered product page");
-            store_stickiness_data(eventData);
+            store_product_visit(eventData);
             break;
         case 'Leaving product page':
             console.log("leaving product page");
@@ -42,40 +43,39 @@ function log_event(event, eventData) {
     }
 }
 
-function store_stickiness_data(eventData) {
-    console.log("in store stickiness, event entered at: " + eventData['enteredAt']);
-    con.connect(function (err) {
+function store_product_visit(eventData) {
+    console.log("in store product visit, event entered at: " + eventData['enteredAt']);
+    var sql = "INSERT INTO product_visits (member, sessionId, productUrl, enteredAt, leftAt) "
+            + "VALUES (" + eventData['member'] + ", '" + eventData['sessionId'] + "', '" + eventData['productUrl'] + "', '" + eventData['enteredAt'].replace(/["']/g, "") + "', '0000-00-00 00:00:00'" + ")";
+    console.log("query: " + sql);
+    con.query(sql, function (err, result) {
         if (err)
             throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO stickiness_data (userId, productId, enteredAt, leftAt) "
-                + "VALUES (1, " + eventData['productId'] + ", '" + eventData['enteredAt'].replace(/["']/g, "") + "', " + "'0000-00-00 00:00:00'" + ")";
-        console.log("query: " + sql);
-        con.query(sql, function (err, result) {
-            if (err)
-                throw err;
-            console.log("1 record inserted");
-        });
+        console.log("1 record inserted");
     });
 }
 
 function update_leaving_time(eventData) {
     console.log("updating leaving time");
+    var sql = "UPDATE product_visits SET leftAt = '" + eventData['leftAt'].replace(/["']/g, "") + "' WHERE "
+            + "sessionId = '" + eventData['sessionId'] + "' AND productUrl = '" + eventData['productUrl']
+            + "' AND " + "date(enteredAt) = '" + dateFormat(new Date(), "yyyy-mm-dd") + "' AND leftAt = '0000-00-00 00:00:00'";
+    console.log("query: " + sql);
+    con.query(sql, function (err, result) {
+        if (err)
+            throw err;
+        console.log(result.affectedRows + " record(s) updated");
+    });
 }
 
 function store_category_visit(eventData) {
     console.log("storing category visits");
-    con.connect(function (err) {
+    var sql = "INSERT INTO category_visits (member, sessionId, categoryUrl) "
+            + "VALUES (" + eventData['member'] + ", '" + eventData['sessionId'] + "', '" + eventData['categoryUrl'] + "')";
+    console.log("query: " + sql);
+    con.query(sql, function (err, result) {
         if (err)
             throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO category_visits (member, sessionId, categoryUrl) "
-                + "VALUES (" + eventData['member'] + ", '" + eventData['sessionId'] + "', '" + eventData['categoryUrl'] + "')";
-        console.log("query: " + sql);
-        con.query(sql, function (err, result) {
-            if (err)
-                throw err;
-            console.log("1 record inserted");
-        });
+        console.log("1 record inserted");
     });
 }
